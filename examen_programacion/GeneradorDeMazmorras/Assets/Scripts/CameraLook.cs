d998b10ac47bc397b0f02e0695ef562fc4571c7d
Elcactusxd <122494817+Elcactusxd@users.noreply.github.com>
2023-04-17 08:21:00 -0600

    Actualizacion de archivos
    Se han subido nuevos archivos.

---
+++ examen_programacion/GeneradorDeMazmorras/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+        //aqui definimos la sensibilidad del raton, el cual controla el movimiento de camara//
+        public float mouseSensitivity = 60f;  
+        public Transform playerBody;  
+        float xRotation = 0f;  
+
+
+        void Update()
+        {
+
+
+            // se defino el movimiento de la camara con base al movimiento del raton//
+
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+
+}
---
+++ examen_programacion/GeneradorDeMazmorras/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    //aqui definimos la velocidad de nuestro personajey declaramos variables para su proximo uso// 
+    public CharacterController controller;
+    public float speed = 10f; 
+    private Vector3 playerPosition;  
+    private Quaternion playerRotation;
+    public float X, Y;  
+
+
+
+    void Update()
+    {
+      
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+     
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
